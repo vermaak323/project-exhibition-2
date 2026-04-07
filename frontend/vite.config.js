2bb75c6c9e927d5c39ffe383940300b503c8911c
Aaryan Kumar Verma <officialak323@gmail.com>
2026-04-07 22:06:27 +0530

    disabled pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,48 +6,48 @@ export default defineConfig({
   base: './', // Ensure relative paths work in subdirectories
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      srcDir: '../pwa',
-      filename: 'sw.js',
-      injectManifest: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-      },
-      devOptions: {
-        enabled: true,
-        type: "module",
-        navigateFallback: 'index.html',
-      },
-      manifest: {
-        name: "SmartBuy AI",
-        short_name: "SmartBuy AI",
-        description: "SmartBuy AI is price Prediction Intelligence App",
-        theme_color: "#EA4335",
-        background_color: "#EA4335",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/pwa-64.png",
-            sizes: "64x64",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   strategies: 'injectManifest',
+    //   srcDir: '../pwa',
+    //   filename: 'sw.js',
+    //   injectManifest: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //     type: "module",
+    //     navigateFallback: 'index.html',
+    //   },
+    //   manifest: {
+    //     name: "SmartBuy AI",
+    //     short_name: "SmartBuy AI",
+    //     description: "SmartBuy AI is price Prediction Intelligence App",
+    //     theme_color: "#EA4335",
+    //     background_color: "#EA4335",
+    //     display: "standalone",
+    //     start_url: "/",
+    //     icons: [
+    //       {
+    //         src: "/pwa-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "/pwa-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "/pwa-64.png",
+    //         sizes: "64x64",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 })
